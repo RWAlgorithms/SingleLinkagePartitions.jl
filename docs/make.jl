e96c742e65abc0f0aabff150a9800f4a00fb8439
Roy <rwang013@uottawa.ca>
2024-01-10 22:31:33 -0500

    enable github docs deploy

--- docs/make.jl
+++ docs/make.jl
@@ -2,32 +2,32 @@ using Documenter
 using SingleLinkagePartitions
 
 
-# # local.
-makedocs(
-    sitename = "SingleLinkagePartitions",
-    modules = [SingleLinkagePartitions],
-    #format = Documenter.HTML(),
-    pages = [
-        "Overview" => "index.md",
-        "Public API" => "api.md",
-        "Demo: chaining & remedy" => "generated/chaining.md",
-    ],
-)
-
-## github.
+# # # local.
 # makedocs(
-#     sitename="SingleLinkagePartitions.jl",
-#     modules=[SingleLinkagePartitions],
-#     format=Documenter.HTML(prettyurls = get(ENV, "CI", nothing)=="true"),
+#     sitename = "SingleLinkagePartitions",
+#     modules = [SingleLinkagePartitions],
+#     #format = Documenter.HTML(),
 #     pages = [
 #         "Overview" => "index.md",
 #         "Public API" => "api.md",
-#         "Demo: image analysis" => "demo.md",
+#         "Demo: chaining & remedy" => "generated/chaining.md",
 #     ],
 # )
-# deploydocs(
-#     repo = "github.com/RoyCCWang/SingleLinkagePartitions.jl.git",
-#     target = "build",
-#     branch = "gh-pages",
-#     versions = ["stable" => "v^", "v#.#" ],
-# )
+
+# github.
+makedocs(
+    sitename="SingleLinkagePartitions.jl",
+    modules=[SingleLinkagePartitions],
+    format=Documenter.HTML(prettyurls = get(ENV, "CI", nothing)=="true"),
+    pages = [
+        "Overview" => "index.md",
+        "Public API" => "api.md",
+        "Demo: image analysis" => "demo.md",
+    ],
+)
+deploydocs(
+    repo = "github.com/RoyCCWang/SingleLinkagePartitions.jl.git",
+    target = "build",
+    branch = "gh-pages",
+    versions = ["stable" => "v^", "v#.#" ],
+)
